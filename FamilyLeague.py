25d4c345306e49fa84da95a01a493eacdf9ff3e6
louier3 <lou3@lourodriguez.com>
2022-10-15 16:33:43 -0400

    what

--- FamilyLeague.py
+++ FamilyLeague.py
@@ -15,7 +15,7 @@ def app():
 
     st.header('Strength of Schedule')
     st.caption('The lower the number, the harder the schedule the team has had. If your average wins against schedule is 1, that means every team in the league would only average 1 win all season with your schedule')
-    df = pd.read_excel(file, sheet_name="Avg Wins Against Schedule")
+    df = pd.read_excel(file, sheet_name="Strength of Schedule")
     df = df.iloc[: , 1:]
     df.index += 1 
     pd.options.mode.chained_assignment = None
