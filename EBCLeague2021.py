1e2f50471c615b68c5bc55b64a28b56796f3a771
louier3 <lou3@lourodriguez.com>
2022-10-22 16:37:38 -0400

    dd

--- EBCLeague2021.py
+++ EBCLeague2021.py
@@ -24,8 +24,8 @@ def app():
     bot30 = round(count * 0.25)
 
     df2 = df.style.apply(lambda x: ["background-color: gold" 
-                          if (int(i.split(" ")[0]) >= top30) 
-                          else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: red; color: white" if (int(i.split(" ")[0]) <= bot30) else "" for i in x], axis = 1, subset=names).apply(lambda x: ["border-style: solid;" if (int(i.split(" ")[0]) == count) else "" for i in x], axis = 1, subset=names)
+                            if (int(i.split(" ")[0]) >= top30 and int(i.split(" ")[0]) < count) 
+                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: red; color: white" if (int(i.split(" ")[0]) <= bot30) else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: goldenrod; color: white" if (int(i.split(" ")[0]) == count) else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: maroon; color: white" if (int(i.split(" ")[0]) == 0) else "" for i in x], axis = 1, subset=names)
 
     st.dataframe(df2, width=2000)
 
