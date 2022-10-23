73cd00e22e6418b39a0ec8f839cbff21f30911ce
louier3 <lou3@lourodriguez.com>
2022-10-23 00:42:01 -0400

    ll

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ pd.options.mode.chained_assignment = None
 files = glob.glob('*.xlsx')
 appended_data = []
 for file in files:
+    print(file.split(".xlsx")[0])
     df = pd.read_excel(file, sheet_name="Louie Power Index")
     appended_data.append(df)
 dfFINAL = pd.concat(appended_data)
@@ -12,4 +13,4 @@ dfFINAL = dfFINAL.iloc[: , 1:]
 dfFINAL.index += 1
 df1 = dfFINAL.sort_values(by=['Louie Power Index (LPI)'], ascending=False)
 
-print(df1)
+# print(df1)
