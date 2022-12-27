cdbbe3e1e95f115b1e7b4af5e2821a103201a098
louier3 <lou3@lourodriguez.com>
2022-12-27 14:43:29 -0500

    final playoff week

--- espl.py
+++ espl.py
@@ -8,10 +8,11 @@ from operator import itemgetter
 start_time = time.time()
 
 # Pennoni Younglings
-# league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX',
+                swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # Family League
-league = League(league_id=1725372613, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+# league = League(league_id=1725372613, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # EBC League
 # league = League(league_id=1118513122, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
@@ -68,7 +69,7 @@ winsMaster = []
 check = True
 # count = 15
 count = 0
-for week in range(1, 17):
+for week in range(1, 18):
     scoreboard = league.scoreboard(week=week)
     if check:
         for sc in scoreboard:
@@ -137,10 +138,10 @@ for i in range(len(keyList)):
     winsList = []
     records = []
 
-df = pd.DataFrame(masterList, columns = names)
-df1 = pd.DataFrame(masterList, columns = names)
+df = pd.DataFrame(masterList, columns=names)
+df1 = pd.DataFrame(masterList, columns=names)
 df1 = df1.set_index("Teams")
-winsDF = pd.DataFrame(winsMaster, columns = names)
+winsDF = pd.DataFrame(winsMaster, columns=names)
 winsDF = winsDF.set_index("Teams")
 
 schedRank = []
@@ -164,7 +165,7 @@ for i in range(len(winsDF)):
     eWins = sum(winsDF.iloc[i]) / len(keyList)
     wins = winsDF.iloc[i][name]
     winPercent = round(wins / count, 3)
-    difference = (totWins/ len(keyList)) - wins
+    difference = (totWins / len(keyList)) - wins
     expWinPercent = round(totWins / count, 3)
     totalWins = round((eWins * multiplier) + wins)
     magicNumber = (expWinPercent * 2) - winPercent
@@ -182,12 +183,13 @@ sortList2 = sorted(rankList, key=itemgetter(1), reverse=True)
 s1 = sorted(schedRank, key=itemgetter(0))
 s2 = sorted(rankList, key=itemgetter(0))
 
-dfLastWeek = pd.read_excel("Pennoni Younglings.xlsx", sheet_name="Louie Power Index")
+dfLastWeek = pd.read_excel("Pennoni Younglings.xlsx",
+                           sheet_name="Louie Power Index")
 # dfLastWeek = dfLastWeek.iloc[: , 1:]
 
 powerRank = []
 for t in range(len(s1)):
-    ranking = (s2[t][1] - s1[t][1]) 
+    ranking = (s2[t][1] - s1[t][1])
     actualWins = df1[s1[t][0]][s1[t][0]]
     # print(s1[t][0])
     # print(dfLastWeek["Teams"])
@@ -195,11 +197,12 @@ for t in range(len(s1)):
     dad = s1[t][0]
     # if s1[t][0] == "DadBod 69ers":
     #     dad = "Your Dad"
-    lastWeek = dfLastWeek.loc[dfLastWeek["Teams"] == dad]["Louie Power Index (LPI)"].item()
+    lastWeek = dfLastWeek.loc[dfLastWeek["Teams"]
+                              == dad]["Louie Power Index (LPI)"].item()
     weekChange = int(ranking) - int(lastWeek)
     if weekChange > 0:
         weekChange = "↑" + str(weekChange)
-    elif(weekChange < 0):
+    elif (weekChange < 0):
         weekChange = "↓" + str(abs(weekChange))
     else:
         weekChange = str(weekChange)
@@ -222,14 +225,17 @@ for s in range(len(sortList2)):
 
 # print(tabulate(sortFinal, headers=["Teams", "Louie Power Index (LPI)"]))
 
-dfSched = pd.DataFrame(sortList, columns = ["Teams", "Avg Wins Against Schedule", "Record"])
-dfRank = pd.DataFrame(sortList2, columns = ["Teams", "Expected Wins", "Difference", "Record"])
-dfLPI = pd.DataFrame(sortLPI, columns = ["Teams", "Louie Power Index (LPI)", "Record", "Change From Last Week"])
-writer = pd.ExcelWriter(leagueName + "PLAYOFFS.xlsx", engine = 'xlsxwriter')
-df1.to_excel(writer, sheet_name = 'Schedule Grid')
-dfSched.to_excel(writer, sheet_name = 'Wins Against Schedule')
-dfRank.to_excel(writer, sheet_name = 'Expected Wins')
-dfLPI.to_excel(writer, sheet_name = 'Louie Power Index')
+dfSched = pd.DataFrame(
+    sortList, columns=["Teams", "Avg Wins Against Schedule", "Record"])
+dfRank = pd.DataFrame(sortList2, columns=[
+                      "Teams", "Expected Wins", "Difference", "Record"])
+dfLPI = pd.DataFrame(sortLPI, columns=[
+                     "Teams", "Louie Power Index (LPI)", "Record", "Change From Last Week"])
+writer = pd.ExcelWriter(leagueName + "PLAYOFFS.xlsx", engine='xlsxwriter')
+df1.to_excel(writer, sheet_name='Schedule Grid')
+dfSched.to_excel(writer, sheet_name='Wins Against Schedule')
+dfRank.to_excel(writer, sheet_name='Expected Wins')
+dfLPI.to_excel(writer, sheet_name='Louie Power Index')
 writer.save()
 
 # writer.close()
--- playoffs.py
+++ playoffs.py
@@ -9,7 +9,8 @@ import random
 start_time = time.time()
 
 # Pennoni Younglings
-league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX',
+                swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # Family League
 # league = League(league_id=1725372613, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
@@ -23,7 +24,7 @@ leagueName = settings.name.replace(" 22/23", "")
 file = leagueName + ".xlsx"
 
 df = pd.read_excel(file, sheet_name="Expected Wins")
-df = df.iloc[: , 1:]
+df = df.iloc[:, 1:]
 
 df2 = pd.read_excel(file, sheet_name="Schedule Grid")
 
@@ -42,4 +43,4 @@ regCount = settings.reg_season_count
 teams = league.teams
 print(teams[0])
 print(teams[0].schedule)
-print(league.scoreboard(week=15))
+print(league.scoreboard(week=16))
