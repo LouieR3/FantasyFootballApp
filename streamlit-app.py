3ec3db88139af363f2b1b37ccdafa3b160c49445
louier3 <lou3@lourodriguez.com>
2023-09-06 19:09:37 -0400

    jus fix it

--- streamlit-app.py
+++ streamlit-app.py
@@ -23,44 +23,42 @@ import PennoniTransportation2022
 import EBCLeague2021
 from espn_api.football import League
 import LPIMasterList
-import st_pages
-from st_pages import Page, Section, add_page_title, show_pages
 
 st.set_page_config(page_title="Louie's Fantasy Football App", layout="wide")
 
-# PAGES = {
-#     "Pennoni Younglings 2022": PennoniYounglings2022,
-#     "Pennoni Transportation 2022": PennoniTransportation2022,
-#     "Prahlad Friends League 2022": PrahladFriendsLeague2022,
-#     "EBC League 2022": EBCleague2022,
-#     "EBC League 2021": EBCLeague2021,
-#     "Family League 2022": FamilyLeague2022,
-#     "LPI Master List": LPIMasterList,
-# }
-# st.sidebar.title('Navigation')
-# selection = st.sidebar.radio("Leagues:", list(PAGES.keys()))
-# page = PAGES[selection]
-# page.app()
+PAGES = {
+    "Pennoni Younglings 2022": PennoniYounglings2022,
+    "Pennoni Transportation 2022": PennoniTransportation2022,
+    "Prahlad Friends League 2022": PrahladFriendsLeague2022,
+    "EBC League 2022": EBCleague2022,
+    "EBC League 2021": EBCLeague2021,
+    "Family League 2022": FamilyLeague2022,
+    "LPI Master List": LPIMasterList,
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Leagues:", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
 
-show_pages(
-    [
-        Page("example_app/PennoniTransportation2022.py", "Home", "🏠"),
-        # Can use :<icon-name>: or the actual icon
-        Page("example_app/PennoniYounglings2022.py", "Example One", ":books:"),
-        # Since this is a Section, all the pages underneath it will be indented
-        # The section itself will look like a normal page, but it won't be clickable
-        Section(name="Cool apps", icon=":pig:"),
-        # The pages appear in the order you pass them
-        Page("example_app/PrahladFriendsLeague2022.py", "Example Four", "📖"),
-        Page("example_app/LPIMasterList.py", "Example Two", "✏️"),
-        Section(name="Other apps", icon=":horse:"),
-        # Will use the default icon and name based on the filename if you don't
-        # pass them
-        Page("example_app/EBCLeague2021.py"),
-        # You can also pass in_section=False to a page to make it un-indented
-        Page("example_app/EBCleague2022.py", "Example Five", "🧰", in_section=False),
-    ]
-)
+# show_pages(
+#     [
+#         Page("example_app/PennoniTransportation2022.py", "Home", "🏠"),
+#         # Can use :<icon-name>: or the actual icon
+#         Page("example_app/PennoniYounglings2022.py", "Example One", ":books:"),
+#         # Since this is a Section, all the pages underneath it will be indented
+#         # The section itself will look like a normal page, but it won't be clickable
+#         Section(name="Cool apps", icon=":pig:"),
+#         # The pages appear in the order you pass them
+#         Page("example_app/PrahladFriendsLeague2022.py", "Example Four", "📖"),
+#         Page("example_app/LPIMasterList.py", "Example Two", "✏️"),
+#         Section(name="Other apps", icon=":horse:"),
+#         # Will use the default icon and name based on the filename if you don't
+#         # pass them
+#         Page("example_app/EBCLeague2021.py"),
+#         # You can also pass in_section=False to a page to make it un-indented
+#         Page("example_app/EBCleague2022.py", "Example Five", "🧰", in_section=False),
+#     ]
+# )
 
 # pr = df.profile_report()
 # st_profile_report(pr)
