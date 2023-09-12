1df6bc43564ec6b9d40f0fcdae6713c0a674fbee
louier3 <lou3@lourodriguez.com>
2023-09-12 13:54:34 -0400

    hope

--- pages/1_🏈_Pennoni_Younglings_2023.py
+++ pages/1_🏈_Pennoni_Younglings_2023.py
@@ -26,14 +26,18 @@ def app():
     top10 = percentList[3]
     bot10 = percentList[4]
 
+    # df2 = df.style.apply(lambda x: ["background-color: khaki" 
+    #                         if (int(i.split(" ")[0]) >= top25 and int(i.split(" ")[0]) < top10) 
+    #                         else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: gold" if (int(i.split(" ")[0]) >= top10 and int(i.split(" ")[0]) < count) 
+    #                         else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: goldenrod" if (int(i.split(" ")[0]) == count) 
+    #                         else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: tomato; color: white" if (int(i.split(" ")[0]) <= bot25 and int(i.split(" ")[0]) > bot10) 
+    #                         else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: red; color: white" if (int(i.split(" ")[0]) <= bot10 and int(i.split(" ")[0]) > 0) 
+    #                         else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: maroon; color: white" if (int(i.split(" ")[0]) == 0) 
+    #                         else "" for i in x], axis = 1, subset=names)
     df2 = df.style.apply(lambda x: ["background-color: khaki" 
                             if (int(i.split(" ")[0]) >= top25 and int(i.split(" ")[0]) < top10) 
-                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: gold" if (int(i.split(" ")[0]) >= top10 and int(i.split(" ")[0]) < count) 
-                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: goldenrod" if (int(i.split(" ")[0]) == count) 
-                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: tomato; color: white" if (int(i.split(" ")[0]) <= bot25 and int(i.split(" ")[0]) > bot10) 
-                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: red; color: white" if (int(i.split(" ")[0]) <= bot10 and int(i.split(" ")[0]) > 0) 
-                            else "" for i in x], axis = 1, subset=names).apply(lambda x: ["background-color: maroon; color: white" if (int(i.split(" ")[0]) == 0) 
-                            else "" for i in x], axis = 1, subset=names)
+                            else "" for i in x], axis=1, subset=names).apply(lambda x: ["background-color: gold" if (int(i.split(" ")[0]) >= top10 and int(i.split(" ")[0]) < count) 
+                            else "" for i in x], axis=1, subset=names).apply(lambda x: ["background-color: goldenrod" if (int(i.split(" ")[0]) == count) else "" for i in x], axis=1, subset=names).apply(lambda x: ["background-color: tomato; color: white" if (int(i.split(" ")[0]) <= bot25 and int(i.split(" ")[0]) > bot10) else "" for i in x], axis=1, subset=names).apply(lambda x: ["background-color: red; color: white" if (int(i.split(" ")[0]) <= bot10 and int(i.split(" ")[0]) > 0) else "" for i in x], axis=1, subset=names).apply(lambda x: ["background-color: maroon; color: white" if (int(i.split(" ")[0]) == 0) else "" for i in x], axis=1, subset=names)
 
     st.dataframe(df2, height=460, width=2000)
 
