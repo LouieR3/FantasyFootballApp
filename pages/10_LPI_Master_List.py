648e539f92ec2ac36221026e9338dfe37f6eca68
louier3 <lou3@lourodriguez.com>
2023-10-25 22:19:34 -0400

    ter

--- pages/10_LPI_Master_List.py
+++ pages/10_LPI_Master_List.py
@@ -19,6 +19,11 @@ def app():
 
     dfFINAL = pd.concat(appended_data)
     dfFINAL = dfFINAL.iloc[: , 1:]
+    first_column = dfFINAL.pop('Owner') 
+  
+    # insert column using insert(position,column_name, 
+    # first_column) function 
+    dfFINAL.insert(1, 'Owner', first_column) 
     # dfFINAL.index += 1
     df1 = dfFINAL.sort_values(by=['Louie Power Index (LPI)'], ascending=False)
     
