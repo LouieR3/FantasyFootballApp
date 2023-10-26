5ce9d1f51bf1c1b82ae6a96af8632a8054eb7b07
louier3 <lou3@lourodriguez.com>
2023-10-26 08:00:18 -0400

    saved changes

--- playoffs.py
+++ playoffs.py
@@ -9,38 +9,128 @@ import random
 start_time = time.time()
 
 # Pennoni Younglings
-league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX',
-                swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+# league = League(league_id=310334683, year=2022, espn_s2='AEC3jc8inPISUEojfHvhzvOsdtsGWNv8sGIxjkBQjQyNQgX%2FDRaM5IKm%2BwyY2guiak1uwiE0xIkP4XEcoTzgLlumNMYgQbnqS3HjnAWI9%2BTZYo2N70ktU9isjCRXRlIvcOFKDV1OmY71%2FgJhMWKodsvEmli0dYCDTMXFF%2Bd7nuCxvGsFSBxV2BPdh8NdKpTEasZN4VhjgG6o9Iczv%2FySPOI9N2x1CGiVJNx8E8rblTk86tPPIr4QdKjYSS7a7Xs2h6KG9i9sLCV%2Be1DJvwtVhgOX',swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # Family League
 # league = League(league_id=1725372613, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # EBC League
-# league = League(league_id=1118513122, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+league = League(league_id=1118513122, year=2022, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 settings = league.settings
+team_owners = [team.owner for team in league.teams]
+team_names = [team.team_name for team in league.teams]
+team_scores = [team.scores for team in league.teams] 
 
-leagueName = settings.name.replace(" 22/23", "")
-file = leagueName + ".xlsx"
+# Assuming you already have data for regular season records and points
+# Create a DataFrame with regular season data
+data = {
+    'Team': team_names,
+    'Wins': [team.wins for team in league.teams],
+    'Losses': [team.losses for team in league.teams],
+    'Points For': [team.points_for for team in league.teams]
+}
+regular_season_df = pd.DataFrame(data)
 
-df = pd.read_excel(file, sheet_name="Expected Wins")
-df = df.iloc[:, 1:]
+# Sort the DataFrame by Wins, then Points For (as a tiebreaker)
+regular_season_df = regular_season_df.sort_values(by=['Wins', 'Points For'], ascending=False)
 
-df2 = pd.read_excel(file, sheet_name="Schedule Grid")
+# Identify playoff teams based on the league's playoff settings
+playoff_team_count = settings.playoff_team_count
 
-names = []
-for col in df2.columns:
-    if col != "Teams":
-        names.append(col)
-percentList = percent(file)
-count = percentList[0]
-top25 = percentList[1]
-bot25 = percentList[2]
-top10 = percentList[3]
-bot10 = percentList[4]
+# Define the number of byes for the top seeds
+num_byes = 2
 
-regCount = settings.reg_season_count
-teams = league.teams
-print(teams[0])
-print(teams[0].schedule)
-print(league.scoreboard(week=16))
+# If there are more teams with byes than playoff spots, adjust the number of byes
+if num_byes > playoff_team_count:
+    num_byes = playoff_team_count
+
+# Get teams with byes
+bye_teams = regular_season_df.head(num_byes)
+
+# Get remaining teams to fill the remaining playoff spots
+remaining_teams = regular_season_df.iloc[num_byes:playoff_team_count]
+
+# Print the standings
+print("Regular Season Standings:")
+print(regular_season_df)
+
+# Print the playoff teams
+print("\nPlayoff Teams:")
+print(remaining_teams)  # Print only the teams competing in the first round
+
+# Print the teams with byes
+print("\nTeams with First-Round Byes:")
+print(bye_teams)
+
+# Determine and print playoff matchups for the remaining teams
+# For simplicity, assuming playoff matchups are fixed (e.g., 3 vs. 6, 4 vs. 5, etc.)
+playoff_matchups = []
+for i in range(num_byes):
+    matchup = f"Matchup {i + 1}: {remaining_teams.iloc[i]['Team']} vs. {remaining_teams.iloc[playoff_team_count - num_byes - i - 1]['Team']}"
+    playoff_matchups.append(matchup)
+
+print("\nFirst Round Playoff Matchups:")
+for matchup in playoff_matchups:
+    print(matchup)
+
+def calculate_playoff_odds(odds_df, num_playoff_teams):
+    # Assuming num_playoff_teams is either 6 or 8
+    if num_playoff_teams == 6:
+        # First round matchups
+        first_round_matchups = [('Place 3', 'Place 6'), ('Place 4', 'Place 5')]
+
+    elif num_playoff_teams == 8:
+        # First round matchups
+        first_round_matchups = [('Place 1', 'Place 8'), ('Place 2', 'Place 7'), ('Place 3', 'Place 6'), ('Place 4', 'Place 5')]
+
+    # Initialize a dictionary to store playoff matchup odds
+    playoff_matchup_odds = {}
+
+    # Calculate odds for first round matchups
+    for matchup in first_round_matchups:
+        team1, team2 = matchup
+        odds_team1 = odds_df.at[team1, 'Chance of making playoffs']
+        odds_team2 = odds_df.at[team2, 'Chance of making playoffs']
+
+        # Probability of team1 winning the matchup
+        prob_team1_wins = (odds_team1 / 100) * (odds_team2 / 100)
+
+        # Probability of team2 winning the matchup
+        prob_team2_wins = (odds_team2 / 100) * (odds_team1 / 100)
+
+        playoff_matchup_odds[matchup] = {
+            team1: prob_team1_wins * 100,
+            team2: prob_team2_wins * 100,
+        }
+
+    # Assuming 1 vs. 4 (or 5) in the next round
+    # Calculate odds for the next round
+    next_round_matchup = ('Place 1', 'Place 4')  # You can change 4 to 5 if 5th place can also make it
+
+    team1, team2 = next_round_matchup
+    odds_team1 = odds_df.at[team1, 'Chance of making playoffs']
+    odds_team2 = odds_df.at[team2, 'Chance of making playoffs']
+
+    # Probability of team1 winning the matchup
+    prob_team1_wins = (odds_team1 / 100) * (odds_team2 / 100)
+
+    # Probability of team2 winning the matchup
+    prob_team2_wins = (odds_team2 / 100) * (odds_team1 / 100)
+
+    playoff_matchup_odds[next_round_matchup] = {
+        team1: prob_team1_wins * 100,
+        team2: prob_team2_wins * 100,
+    }
+
+    return playoff_matchup_odds
+
+# Calculate playoff odds for 6-team playoffs
+playoff_odds_6 = calculate_playoff_odds(odds_df, num_playoff_teams=6)
+print("Playoff Odds for 6-Team Playoffs:")
+print(playoff_odds_6)
+
+# Calculate playoff odds for 8-team playoffs
+# playoff_odds_8 = calculate_playoff_odds(odds_df, num_playoff_teams=8)
+# print("\nPlayoff Odds for 8-Team Playoffs:")
+# print(playoff_odds_8)
--- test.py
+++ test.py
@@ -37,20 +37,23 @@ start_time = time.time()
 #     count += 1
 # print()
 # Pennoni Younglings
-# league = League(league_id=310334683, year=2023, espn_s2='AEAfTKrAtdKbMxfMyenv9DJYTiU3C8VnQUxCN2Th4N3NOlCz9K%2FI6Q7ivooWGDq9sntbsoARrL32sOaiwyJFrdsaLBZO1AZzNTIkB00%2BX4fqBIVFgMRZe96xpKk72nJ9kfqgSf7rcQDIsQ6SDXU4fT6O0v9er%2BRsJWvZyLF4qPJ5tfqwvPMFgRBjX4ve1uzcAwgaDkVWSQwE27XWMyHoAYOzMU89l7vcEjtIWsdsdJVmjSUEHBSuK1wuMXeOG0GG54S4ckEu2jJB4QRFN%2FqMyEDM', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+league = League(league_id=310334683, year=2023, espn_s2='AEAfTKrAtdKbMxfMyenv9DJYTiU3C8VnQUxCN2Th4N3NOlCz9K%2FI6Q7ivooWGDq9sntbsoARrL32sOaiwyJFrdsaLBZO1AZzNTIkB00%2BX4fqBIVFgMRZe96xpKk72nJ9kfqgSf7rcQDIsQ6SDXU4fT6O0v9er%2BRsJWvZyLF4qPJ5tfqwvPMFgRBjX4ve1uzcAwgaDkVWSQwE27XWMyHoAYOzMU89l7vcEjtIWsdsdJVmjSUEHBSuK1wuMXeOG0GG54S4ckEu2jJB4QRFN%2FqMyEDM', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # Family League
 # league = League(league_id=1725372613, year=2023, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # EBC League
-league = League(league_id=1118513122, year=2023, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
+# league = League(league_id=1118513122, year=2023, espn_s2='AEBxvJwo9gYK1pk%2B3S36%2FFZS5WVqYHsY3l6QKMwy538U7Q%2BbCKt237iKEykfAurrxK0T%2B4M%2FhsXk6t2oLyY%2Fle6b5DUKWvsi1ZXzyMRzW7mBevrrtS1Uhyr7KNCPzM0ccOB1Daw4Xv%2FnY9b9KiMxPCRNcosaDEkZfjR%2ByCcF2KtYqhZ90gEfrdWGG4GlVjpMw7Ve4fL7V0mHDp3NgozRqkB7cZH2dZ0fOjF%2BPMwo9hQZ3V3R9jQdvAp2f3Dx2nbDiG%2Fi9oqM9cN1U87DEjHRu7CI', swid='{4656A2AD-A939-460B-96A2-ADA939760B8B}')
 
 # Pennoni Transportation
 # league = League(league_id=1339704102, year=2023, espn_s2='AEBnVIPLGawrfX3pYmFejB2uTpTrDT5gKM7jbAqOtvaNBfAF0muAaPFFZBzwevb6Robdlp8Ruok9B8MFrXj6DEDW6m3zhlv0j9q%2BSVF446Q%2BU3ui%2F2mNHJK34K7mlc9dhW03a4HgrNWR4GDPukRdI5orkAF3Kl5KeDamvTff%2BaIlroUAgYyKLzQyEueU%2BLCCn4Jwb5ZLPBFSW00QQ3UbYc9tGwNeDZAKIiEEfd%2FQiKWXYfQnwep48PkunIN5%2FhYoa5MsjfG6jMhQAX22al5F%2F%2Fpuq6X7ei4emvlW3KAUbUMiY%2Bx4ViHMbWOcmrwkMPPFFqOsW8%2BkFK%2B1C40tt7Z3%2BaY1', swid='{634597F9-8435-46D1-9314-B554E8B4BB2A}')
 
 # Prahlad Friends League
 # league = League(league_id=1781851, year=2023, espn_s2='AEBnVIPLGawrfX3pYmFejB2uTpTrDT5gKM7jbAqOtvaNBfAF0muAaPFFZBzwevb6Robdlp8Ruok9B8MFrXj6DEDW6m3zhlv0j9q%2BSVF446Q%2BU3ui%2F2mNHJK34K7mlc9dhW03a4HgrNWR4GDPukRdI5orkAF3Kl5KeDamvTff%2BaIlroUAgYyKLzQyEueU%2BLCCn4Jwb5ZLPBFSW00QQ3UbYc9tGwNeDZAKIiEEfd%2FQiKWXYfQnwep48PkunIN5%2FhYoa5MsjfG6jMhQAX22al5F%2F%2Fpuq6X7ei4emvlW3KAUbUMiY%2Bx4ViHMbWOcmrwkMPPFFqOsW8%2BkFK%2B1C40tt7Z3%2BaY1', swid='{634597F9-8435-46D1-9314-B554E8B4BB2A}')
-
+print(league.power_rankings(week=5))
+settings = league.settings
+print(settings.reg_season_count)
+print(settings.playoff_team_count)
 
 # team_owners = [team.owner for team in league.teams]
 # print(team_owners)
@@ -61,17 +64,6 @@ league = League(league_id=1118513122, year=2023, espn_s2='AEBxvJwo9gYK1pk%2B3S36
 # 
 # league = "Pennoni Younglings 2023"
 # league = "EBC League 2023"
-league = "Family League 2023"
-file = league + ".xlsx"
-df = pd.read_excel(file, sheet_name="Louie Power Index")
-print(df)
-record_split = df['Record'].iloc[0].split('-')
 
-# Convert to ints
-record_nums = [int(num) for num in record_split] 
 
-# Sum 
-record_sum = sum(record_nums)
-
-print(record_sum)
 print("--- %s seconds ---" % (time.time() - start_time))
