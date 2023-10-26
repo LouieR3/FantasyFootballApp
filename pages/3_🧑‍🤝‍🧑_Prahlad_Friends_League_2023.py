0bcf053872959fff15bb01422e59614361484dcb
louier3 <lou3@lourodriguez.com>
2023-10-25 22:34:46 -0400

    try

--- pages/3_🧑‍🤝‍🧑_Prahlad_Friends_League_2023.py
+++ pages/3_🧑‍🤝‍🧑_Prahlad_Friends_League_2023.py
@@ -95,4 +95,12 @@ def app():
     df3 = df.style.background_gradient(subset=['Louie Power Index (LPI)'])
     st.dataframe(df3)
 
+    st.header('Biggest LPI Upsets')
+    # st.write('The LPI shows which direction teams should trend - high scores but worse records suggest improvement ahead. Low scores but better records indicate expected decline.')
+    df = pd.read_excel(file, sheet_name="Biggest Upsets")
+    df = df.iloc[: , 1:]
+    df.index += 1
+    df3 = df.style.background_gradient(subset=['LPI Difference'])
+    st.dataframe(df3)
+
 app()
