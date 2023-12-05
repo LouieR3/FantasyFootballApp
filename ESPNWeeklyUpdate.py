45bb0a647aeab52e44cabb044aa09eec447e6733
louier3 <lou3@lourodriguez.com>
2023-12-05 09:02:11 -0500

    week 13

--- ESPNWeeklyUpdate.py
+++ ESPNWeeklyUpdate.py
@@ -64,7 +64,7 @@ if current_week is None:
     current_week = settings.reg_season_count
 elif current_week != settings.reg_season_count:
   current_week -= 1
-# current_week = 17
+current_week = 13
 
 # Store data in DataFrames 
 scores_df = pd.DataFrame(team_scores, index=team_names)
