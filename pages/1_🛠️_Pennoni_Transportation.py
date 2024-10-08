ba3d64d1616de0b5c2edd57e041bda2b26b3d501
louier3 <lou3@lourodriguez.com>
2024-10-08 10:27:52 -0400

    fixed

--- pages/1_🛠️_Pennoni_Transportation.py
+++ pages/1_🛠️_Pennoni_Transportation.py
@@ -5,8 +5,8 @@ def app():
     from calcPercent import percent
     
     # Initialize the dropdown for year selection
-    year_options = ['2022', '2023', '2024']
-    selected_year = st.selectbox("Select Year", year_options, index=2)  # Defaults to 2024
+    year_options = ['2022', '2024']
+    selected_year = st.selectbox("Select Year", year_options, index=1)  # Defaults to 2024
     league = f"0755 Fantasy Football {selected_year}"
 
     st.title("🛠️ " + league)
