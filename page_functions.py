6e1932d05c174208913dd4b2836ec655dae78771
louier3 <lou3@lourodriguez.com>
2025-09-05 11:43:11 -0400

    sgfd

--- page_functions.py
+++ page_functions.py
@@ -211,6 +211,7 @@ def display_playoff_odds(file):
     styled_df = formatted_df.style.set_properties(**{'background-color': 'lightgray'}, subset=slice_)
     
     df_names = pd.read_excel(file, sheet_name="Wins Against Schedule")
+    df_names.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     # Display the styled DataFrame
     df_names = df_names.set_index("Teams")
     pd.options.mode.chained_assignment = None
@@ -239,6 +240,7 @@ def display_lpi_by_week(file):
     df.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     df_names = pd.read_excel(file, sheet_name="Wins Against Schedule")
     # Display the styled DataFrame
+    df_names.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     df_names = df_names.set_index("Teams")
     pd.options.mode.chained_assignment = None
     names = []
@@ -274,6 +276,7 @@ def display_lpi(file):
     df3 = df.style.background_gradient(subset=['Louie Power Index (LPI)'])
     df_names = pd.read_excel(file, sheet_name="Wins Against Schedule")
     # Display the styled DataFrame
+    df_names.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     df_names = df_names.set_index("Teams")
     pd.options.mode.chained_assignment = None
     names = []
@@ -306,6 +309,7 @@ def display_biggest_lpi_upsets(file):
     df3 = df.style.background_gradient(subset=['LPI Difference'])
     df_names = pd.read_excel(file, sheet_name="Wins Against Schedule")
     # Display the styled DataFrame
+    df_names.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     df_names = df_names.set_index("Teams")
     pd.options.mode.chained_assignment = None
     names = []
@@ -351,6 +355,7 @@ def display_lifetime_record(file, league_id, espn_s2, swid, year_options):
     df4 = lifetime_record_df.style.background_gradient(subset=['Win Percentage'])
     df_names = pd.read_excel(file, sheet_name="Wins Against Schedule")
     # Display the styled DataFrame
+    df_names.rename(columns={'Unnamed: 0': 'Teams'}, inplace=True)
     df_names = df_names.set_index("Teams")
     pd.options.mode.chained_assignment = None
     names = []
