7bfe4d997489636de8e51e40db1023705f53c048
louier3 <lou3@lourodriguez.com>
2025-11-22 11:37:19 -0500

    asfd

--- page_functions.py
+++ page_functions.py
@@ -357,9 +357,10 @@ def display_remaining_schedule_difficulty(file):
     df = df.iloc[:, 1:]
     df.index += 1
 
-    # Format specific columns to show one decimal point
-    columns_to_format = ['Avg_Points_For', 'Win_Pct', 'Avg_Opp_Points_For', 'Avg_Opp_Win_Pct', 'Avg_Opp_LPI']
-    df[columns_to_format] = df[columns_to_format].round(1)
+    # Format specific columns
+    columns_to_format = ['Avg_Points_For', 'Avg_Opp_Points_For', 'Avg_Opp_Win_Pct', 'Avg_Opp_LPI']
+    df[columns_to_format] = df[columns_to_format].applymap(lambda x: f"{x:.1f}")  # Format to 1 decimal place
+    df['Win_Pct'] = df['Win_Pct'].apply(lambda x: f"{x:.3f}")  # Format to 3 decimal places
 
     # df['Avg LPI of Remaining Opponents'] = (df['Avg LPI of Remaining Opponents']
     #                                       .round(2))
