91b95b3dfb320848d1df3434a8b7c2a911108368
louier3 <lou3@lourodriguez.com>
2025-11-22 10:08:21 -0500

    lpi

--- page_functions.py
+++ page_functions.py
@@ -664,9 +664,8 @@ def display_lpi(league_id, espn_s2, swid, file):
     - pd.DataFrame: The Louie Power Index DataFrame.
     """
     st.header('The Louie Power Index (LPI)')
-    st.write('The Louie Power Index compares Expected Wins and Strength of Schedule to produce a strength of schedule adjusted score.')
-    st.write('Positive scores indicate winning against tough schedules. Negative scores mean losing with an easy schedule. Higher scores are better. Scores near zero are neutral.')
-    st.write('The LPI shows which direction teams should trend - high scores but worse records suggest improvement ahead. Low scores but better records indicate expected decline.')
+    # st.write('The Louie Power Index compares Expected Wins and Strength of Schedule to produce a strength of schedule adjusted score.')
+    st.write('The LPI shows which direction teams should trend - high scores but worse records suggest improvement ahead. Low scores but better records indicate expected decline. Positive scores indicate winning against tough schedules. Negative scores mean losing with an easy schedule. Higher scores are better. Scores near zero are neutral.')
     df = pd.read_excel(file, sheet_name="Louie Power Index")
     
     df = df.iloc[: , 1:]
